63972633db98d10a80a168cb5e10d08efd5cda10
meocutee123 <49399040+meocutee123@users.noreply.github.com>
2020-04-19 10:38:05 +0700

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,36 @@
+﻿using BaiTap2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTap2.Controllers
+{
+    public class StudentController : Controller
+    {
+        // Registration action
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        // Registration post action
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration(StudentModel sm)
+        {
+            if (ModelState.IsValid)
+            {
+
+                using (StudentInformationDataBaseEntities db = new StudentInformationDataBaseEntities())
+                {
+                    db.Students.Add(sm);
+                    db.SaveChanges();
+
+                }
+            }
+            return View(sm);
+        }
+
+    }
+}
---
+++ Models/StudentModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BaiTap2.Models
+{
+    public class StudentModel
+    {
+        [Display(Name = "Student number")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Student number required!")]
+        public string studentNub { get; set; }
+        [Display(Name = "Student name")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Student name required!")]
+        public string studentName { get; set; }
+        [Display(Name = "Student score")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Student score required!")]
+        public float studentScore { get; set; }
+    }
+}
